2014ac13a532542d4d5fbf2931a8dd00e83b794d
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-09 01:05:38 -0300

    Update index.js

--- src/routes/ads/index.js
+++ src/routes/ads/index.js
@@ -4,11 +4,23 @@ const auth = require('../../middlewares/auth');
 const createController = require('../../controllers/ads/create.controller');
 const listController = require('../../controllers/ads/list.controller');
 const showController = require('../../controllers/ads/show.controller');
+const cityController = require('../../controllers/ads/city.controller');
+const brandController = require('../../controllers/ads/brand.controller');
+const modelController = require('../../controllers/ads/model.controller');
 
 const router = express.Router();
 
 router.post('/', auth, createController);
 router.get('/', listController);
 router.get('/carro/:slug', showController);
+
+// SEO por cidade
+router.get('/cidade/:city-:state', cityController);
+
+// SEO por marca
+router.get('/cidade/:city-:state/:brand', brandController);
+
+// SEO por modelo
+router.get('/cidade/:city-:state/:brand/:model', modelController);
 
 module.exports = router;
