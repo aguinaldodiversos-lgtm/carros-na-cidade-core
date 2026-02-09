2c468f7dc0feceab5c0641fde1379334c8e16d70
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-09 08:14:39 -0300

    Update index.js

--- src/routes/ads/index.js
+++ src/routes/ads/index.js
@@ -7,12 +7,28 @@ const showController = require('../../controllers/ads/show.controller');
 const cityController = require('../../controllers/ads/city.controller');
 const brandController = require('../../controllers/ads/brand.controller');
 const modelController = require('../../controllers/ads/model.controller');
+const searchController = require('../../controllers/ads/search.controller');
 
 const router = express.Router();
+
+/* =====================================================
+   ROTAS DE BUSCA
+===================================================== */
+
+// Busca com filtros (home, filtros, ofertas)
+router.get('/search', searchController);
+
+/* =====================================================
+   ROTAS PRINCIPAIS
+===================================================== */
 
 router.post('/', auth, createController);
 router.get('/', listController);
 router.get('/carro/:slug', showController);
+
+/* =====================================================
+   ROTAS SEO
+===================================================== */
 
 // SEO por cidade
 router.get('/cidade/:city-:state', cityController);
