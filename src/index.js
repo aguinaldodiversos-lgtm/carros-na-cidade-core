b32eda0842b65074691652a5010640f0d72da011
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-09 21:44:32 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
-
 const adsRoutes = require('./routes/ads');
 const authRoutes = require('./routes/auth');
 const paymentRoutes = require('./routes/payments');
