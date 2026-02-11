82d2815e6b5378c5611cbd8282c305602a856a1e
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-11 13:02:12 -0300

    Update autopilot.worker.js

--- src/workers/autopilot.worker.js
+++ src/workers/autopilot.worker.js
@@ -3,10 +3,10 @@ const { runAutopilot } = require("../services/autopilot.service");
 function startAutopilotWorker() {
   console.log("🤖 Autopilot worker iniciado");
 
-  // roda a cada 12 horas
-  setInterval(() => {
-    runAutopilot();
-  }, 1000 * 60 * 60 * 12);
+  // roda a cada 30 minutos
+  setInterval(async () => {
+    await runAutopilot();
+  }, 1000 * 60 * 30);
 }
 
 module.exports = { startAutopilotWorker };
