8d2404b8e0c5fe26bb40da0b160285b14072b89e
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-11 00:56:58 -0300

    Update index.js

--- src/routes/analytics/index.js
+++ src/routes/analytics/index.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const alertsController = require("../../controllers/analytics/alerts.controller");
+const opportunitiesController = require("../../controllers/analytics/opportunities.controller");
 
 const router = express.Router();
 
 router.get("/alerts", alertsController);
+router.get("/opportunities", opportunitiesController);
 
 module.exports = router;
