fb5bda5ee1fd922e6d497420cca6668a64dd1256
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-14 00:46:15 -0300

    Update index.js

--- src/routes/ads/index.js
+++ src/routes/ads/index.js
@@ -1,45 +1,47 @@
-const express = require('express');
-const auth = require('../../middlewares/auth');
+const express = require("express");
+const auth = require("../../middlewares/auth");
 
-const createController = require('../../controllers/ads/create.controller');
-const listController = require('../../controllers/ads/list.controller');
-const showController = require('../../controllers/ads/show.controller');
-const cityController = require('../../controllers/ads/city.controller');
-const brandController = require('../../controllers/ads/brand.controller');
-const modelController = require('../../controllers/ads/model.controller');
-const searchController = require('../../controllers/ads/search.controller');
-
-const { Pool } = require("pg");
+const createController = require("../../controllers/ads/create.controller");
+const listController = require("../../controllers/ads/list.controller");
+const showController = require("../../controllers/ads/show.controller");
+const cityController = require("../../controllers/ads/city.controller");
+const brandController = require("../../controllers/ads/brand.controller");
+const modelController = require("../../controllers/ads/model.controller");
+const searchController = require("../../controllers/ads/search.controller");
 
 const router = express.Router();
-
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: { rejectUnauthorized: false },
-});
 
 /* =====================================================
    ROTAS DE BUSCA
 ===================================================== */
 
 // Busca com filtros (home, filtros, ofertas)
-router.get('/search', searchController);
+router.get("/search", searchController);
 
 /* =====================================================
    ROTAS PRINCIPAIS
 ===================================================== */
 
-router.post('/', auth, createController);
-router.get('/', listController);
-router.get('/carro/:slug', showController);
+// Criar anúncio
+router.post("/", auth, createController);
+
+// Listar anúncios
+router.get("/", listController);
+
+// Detalhe do anúncio
+router.get("/carro/:slug", showController);
 
 /* =====================================================
-   MELHORES OFERTAS DA CIDADE
+   ROTAS SEO
 ===================================================== */
 
-router.get('/best-deals', async (req, res) => {
-  try {
-    const { city, limit = 10 } = req.query;
+// SEO por cidade
+router.get("/cidade/:city-:state", cityController);
 
-    if (!city) {
-      return res.status(400).json({ er
+// SEO por marca
+router.get("/cidade/:city-:state/:brand", brandController);
+
+// SEO por modelo
+router.get("/cidade/:city-:state/:brand/:model", modelController);
+
+module.exports = router;
