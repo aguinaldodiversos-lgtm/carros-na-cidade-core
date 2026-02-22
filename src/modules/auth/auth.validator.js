67dd6dbc738a9f03c1356bda7227468a711af86f
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-22 12:34:10 -0300

    Create auth.validator.js

---
+++ src/modules/auth/auth.validator.js
@@ -0,0 +1,12 @@
+// src/modules/auth/auth.validator.js
+
+import { z } from "zod";
+
+export const loginSchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+});
+
+export const refreshSchema = z.object({
+  refreshToken: z.string().min(10),
+});
