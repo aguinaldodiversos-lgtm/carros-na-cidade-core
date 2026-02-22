aaab30dbf1546dde371cf70081629dd8bef12969
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-22 14:43:44 -0300

    Create googleAds.service.js

---
+++ src/modules/external/googleAds.service.js
@@ -0,0 +1,10 @@
+// src/modules/external/googleAds.service.js
+
+export async function fetchGoogleAdsData() {
+  // Placeholder até ativar credenciais reais
+  return {
+    cpc: 0,
+    roas: 0,
+    conversions: 0,
+  };
+}
