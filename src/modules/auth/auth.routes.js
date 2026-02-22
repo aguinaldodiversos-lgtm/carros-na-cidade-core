d22687461616028d58b6cd8afcf0b45cc094907b
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-22 12:32:27 -0300

    Update auth.routes.js

--- src/modules/auth/auth.routes.js
+++ src/modules/auth/auth.routes.js
@@ -2,9 +2,15 @@
 
 import express from "express";
 import * as authController from "./auth.controller.js";
+import { validate } from "../../shared/middlewares/validate.middleware.js";
+import {
+  loginSchema,
+  refreshSchema,
+} from "./auth.validator.js";
 
 const router = express.Router();
 
-router.post("/login", authController.login);
+router.post("/login", validate(loginSchema), authController.login);
+router.post("/refresh", validate(refreshSchema), authController.refresh);
 
 export default router;
