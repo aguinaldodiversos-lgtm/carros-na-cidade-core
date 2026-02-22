0878d786df5ebe18b73e7505430e731fed75b7db
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-02-22 14:44:14 -0300

    Create metaAds.service.js

---
+++ src/modules/external/metaAds.service.js
@@ -0,0 +1,9 @@
+// src/modules/external/metaAds.service.js
+
+export async function fetchMetaAdsData() {
+  // Placeholder até configurar app Meta
+  return {
+    reach: 0,
+    cost_per_result: 0,
+  };
+}
