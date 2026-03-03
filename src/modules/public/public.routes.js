0eef7e8548289e65cc101f2457bbd2883e1c6bf9
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-03-02 22:38:10 -0300

    Update public.routes.js

--- src/modules/public/public.routes.js
+++ src/modules/public/public.routes.js
@@ -1,10 +1,13 @@
-// src/modules/public/public.routes.js
-
 import express from "express";
 import { getHomeData } from "./public.controller.js";
+import { cacheGet } from "../../shared/cache/cache.middleware.js";
 
 const router = express.Router();
 
-router.get("/home", getHomeData);
+router.get(
+  "/home",
+  cacheGet({ prefix: "home", ttlSeconds: 60, varyBy: ["query"] }),
+  getHomeData
+);
 
 export default router;
