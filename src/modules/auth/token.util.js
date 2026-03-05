154d381b815e4b4e6df37f0eb5e75dff763aaed7
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-03-04 22:54:57 -0300

    Create token.util.js

---
+++ src/modules/auth/token.util.js
@@ -0,0 +1,29 @@
+import jwt from "jsonwebtoken";
+import crypto from "crypto";
+
+export function generateAccessToken(user) {
+  return jwt.sign(
+    {
+      sub: user.id,
+      email: user.email,
+      type: "access"
+    },
+    process.env.JWT_SECRET,
+    { expiresIn: "15m" }
+  );
+}
+
+export function generateRefreshToken(user) {
+  return jwt.sign(
+    {
+      sub: user.id,
+      type: "refresh"
+    },
+    process.env.JWT_REFRESH_SECRET,
+    { expiresIn: "7d" }
+  );
+}
+
+export function hashToken(token) {
+  return crypto.createHash("sha256").update(token).digest("hex");
+}
