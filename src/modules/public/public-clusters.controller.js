071836d63311ee824ee9303184d1a8c81644b19b
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-03-06 22:31:20 -0300

    Create public-clusters.controller.js

---
+++ src/modules/public/public-clusters.controller.js
@@ -0,0 +1,76 @@
+import { getCityPublicPage } from "../../read-models/cities/city-public.service.js";
+import { getCityBrandPage } from "../../read-models/cities/city-brand.service.js";
+import { getCityModelPage } from "../../read-models/cities/city-model.service.js";
+import {
+  getCityOpportunityPage,
+  getCityBelowFipePage,
+} from "../../read-models/cities/city-opportunity.service.js";
+
+export async function getCityPage(req, res, next) {
+  try {
+    const data = await getCityPublicPage(req.params.slug);
+
+    res.json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getCityBrandClusterPage(req, res, next) {
+  try {
+    const data = await getCityBrandPage(req.params.slug, req.params.brand);
+
+    res.json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getCityModelClusterPage(req, res, next) {
+  try {
+    const data = await getCityModelPage(
+      req.params.slug,
+      req.params.brand,
+      req.params.model
+    );
+
+    res.json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getCityOpportunityClusterPage(req, res, next) {
+  try {
+    const data = await getCityOpportunityPage(req.params.slug);
+
+    res.json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
+
+export async function getCityBelowFipeClusterPage(req, res, next) {
+  try {
+    const data = await getCityBelowFipePage(req.params.slug);
+
+    res.json({
+      success: true,
+      data,
+    });
+  } catch (err) {
+    next(err);
+  }
+}
