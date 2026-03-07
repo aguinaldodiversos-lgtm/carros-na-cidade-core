2db88859017990715873c3d8bea40c1125d992a4
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-03-07 20:30:01 -0300

    Update facets.service.js

--- src/modules/ads/facets.service.js
+++ src/modules/ads/facets.service.js
@@ -1,6 +1,8 @@
+// src/modules/ads/facets.service.js
+
 import { getFacetsWithFilters } from "./filters/ads-filter.service.js";
 
 export async function getFacets(filters = {}) {
-  const result = await getFacetsWithFilters(filters);
+  const result = await getFacetsWithFilters(filters, { safeMode: true });
   return result.facets;
 }
