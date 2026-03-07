210781332fbfce2e865f0641a8d1ec8cd9d4d078
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-03-06 21:48:42 -0300

    Create index.js

---
+++ src/modules/seo/index.js
@@ -0,0 +1,6 @@
+export * as seoPagesService from "./pages/seo-pages.service.js";
+export * as seoContentService from "./content/seo-content.service.js";
+export * as seoMetricsService from "./metrics/seo-metrics.service.js";
+export * as seoRefreshService from "./refresh/seo-refresh.service.js";
+export * as seoLinkingService from "./linking/seo-linking.service.js";
+export * as seoIndexingService from "./indexing/seo-indexing.service.js";
