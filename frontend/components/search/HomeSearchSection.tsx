c97654f126e23f9a8ab451783989683da38b7027
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-03-07 21:52:52 -0300

    Update HomeSearchSection.tsx

--- frontend/components/search/HomeSearchSection.tsx
+++ frontend/components/search/HomeSearchSection.tsx
@@ -3,15 +3,18 @@
 "use client";
 
 import { SmartVehicleSearch } from "./SmartVehicleSearch";
+import { TerritorialEntrySection } from "../home/TerritorialEntrySection";
 
 export function HomeSearchSection() {
   return (
     <section className="w-full">
-      <div className="mx-auto max-w-5xl">
+      <div className="mx-auto max-w-6xl">
         <SmartVehicleSearch
           placeholder="Ex.: Corolla até 110 mil em Campinas"
           resultsBasePath="/anuncios"
         />
+
+        <TerritorialEntrySection />
       </div>
     </section>
   );
