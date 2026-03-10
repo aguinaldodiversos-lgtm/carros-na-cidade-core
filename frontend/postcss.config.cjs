f3faf14c860011582714ea9880fe8d215017a120
Aguinaldo <aguinaldo.diversos@gmail.com>
2026-03-10 20:56:29 -0300

    Fix corrupted postcss.config.cjs

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
-// frontend/postcss.config.cjs
-=======
 /** @type {import('postcss-load-config').Config} */
->>>>>>> 23e9335 (Fix postcss.config.cjs (remove conflict markers))
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
-<<<<<<< HEAD
 };
-=======
-};
->>>>>>> 23e9335 (Fix postcss.config.cjs (remove conflict markers))
