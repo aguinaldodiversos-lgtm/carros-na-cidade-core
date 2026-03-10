46770c61503299baff8268b68da9602b05e7bb59
Aguinaldo <aguinaldo.diversos@gmail.com>
2026-03-10 20:31:07 -0300

    Fix postcss.config.cjs (remove conflict markers)

--- frontend/postcss.config.cjs
+++ frontend/postcss.config.cjs
@@ -1,7 +1,15 @@
+<<<<<<< HEAD
 // frontend/postcss.config.cjs
+=======
+/** @type {import('postcss-load-config').Config} */
+>>>>>>> 23e9335 (Fix postcss.config.cjs (remove conflict markers))
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
+<<<<<<< HEAD
 };
+=======
+};
+>>>>>>> 23e9335 (Fix postcss.config.cjs (remove conflict markers))
