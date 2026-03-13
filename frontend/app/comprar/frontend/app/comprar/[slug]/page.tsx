81aac7ad1630ef1ccb652ac636a0bb89da4c18c3
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-03-13 20:43:43 -0300

    Add VehicleDetailsRoute for ad details page

---
+++ frontend/app/comprar/frontend/app/comprar/[slug]/page.tsx
@@ -0,0 +1,16 @@
+import AdDetailsPage from "../../../components/ads/AdDetailsPage";
+import { getAdDetails } from "../../../lib/ads/get-ad-details";
+
+export const revalidate = 60;
+
+type PageProps = {
+  params: {
+    slug: string;
+  };
+};
+
+export default async function VehicleDetailsRoute({ params }: PageProps) {
+  const ad = await getAdDetails(params.slug);
+
+  return <AdDetailsPage ad={ad} />;
+}
