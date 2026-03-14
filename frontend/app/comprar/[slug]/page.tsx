03f39a8196d05d5da00055bffdf5f759cdf8b8d0
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-03-14 16:23:26 -0300

    Update page.tsx

--- frontend/app/comprar/[slug]/page.tsx
+++ frontend/app/comprar/[slug]/page.tsx
@@ -1,5 +1,5 @@
-import AdDetailsPage from "@/components/ads/AdDetailsPage";
-import { getAdDetails } from "@/lib/ads/get-ad-details";
+import AdDetailsPage from "../../../components/ads/AdDetailsPage";
+import { getAdDetails } from "../../../lib/ads/get-ad-details";
 
 export const revalidate = 60;
 
@@ -11,5 +11,6 @@ type PageProps = {
 
 export default async function VehicleDetailsRoute({ params }: PageProps) {
   const ad = await getAdDetails(params.slug);
+
   return <AdDetailsPage ad={ad} />;
 }
