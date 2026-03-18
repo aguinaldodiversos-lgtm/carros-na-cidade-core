40bffd2c9f6dd085dbc56ad379ba47ee5bf84c53
aguinaldodiversos-lgtm <aguinaldo.diversos@gmail.com>
2026-03-17 23:30:43 -0300

    Migrate db.js to use infrastructure database module
    Refactor database connection to use new infrastructure module.

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,14 @@
-const { Pool } = require('pg');
+/**
+ * Shim de compatibilidade.
+ * Mantém módulos legados apontando para a infraestrutura única de banco.
+ */
+import pool, {
+  query,
+  withTransaction,
+  healthcheck,
+  closeDatabasePool,
+  getPoolConfig,
+} from "../infrastructure/database/db.js";
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: process.env.NODE_ENV === 'production'
-    ? { rejectUnauthorized: false }
-    : false,
-});
-
-module.exports = pool;
+export { pool, query, withTransaction, healthcheck, closeDatabasePool, getPoolConfig };
+export default pool;
